5f7c3c3f451fa031e23968cead1f52ac2de56f92
Nico <nicolas.roitero@roihunter.com>
2022-10-29 05:51:24 +0200

    initial commit

---
+++ script.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+CA_CRT=$(cat /var/run/secrets/kubernetes.io/serviceaccount/ca.crt |base64 -w0)
+TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token )
+mkdir -p /home/coder/.kube
+cat >/home/coder/.kube/config <<EOL
+apiVersion: v1
+clusters:
+- cluster:
+    certificate-authority-data: ${CA_CRT}
+    server: https://${KUBERNETES_PORT_443_TCP_ADDR}:443
+  name: default
+contexts:
+- context:
+    cluster: default
+    user: default
+  name: default
+current-context: default
+kind: Config
+preferences: {}
+users:
+- name: default
+  user:
+    token: ${TOKEN}
+EOL
+
+
+
+dumb-init fixuid -q /usr/bin/code-server --bind-addr 0.0.0.0:8080 --auth none .
